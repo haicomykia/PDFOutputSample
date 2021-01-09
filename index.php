05de959323750683e51f8c898ed35b88f382b550
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-01-10 02:26:32 +0900

    mutationObserverのテストコード

--- index.php
+++ index.php
@@ -14,161 +14,12 @@
 </head>
 <body class="bg-light">
   <div class="container-lg bg-white">
-    <div class="row">
-      <div class="col-2">
-        <input type="text" name="order_date" id="order-date" class="form-control order-datepicker">
-      </div>
-      <div class="col-2">
-        <input type="text" name="delivery_date" id="delivery-date" class="form-control delivery-datepicker">
-      </div>
-    </div>
-    <ol class="list-unstyled">
-      <li class="my-3">
-        <div class="row ">
-          <div class="col-3 d-flex justify-content-center">
-            <!-- 画像ファイル、PDF以外（フォールバック） -->
-            <img src="./img/doc_icon.svg" alt="" class="img-thumbnail border-0">
-          </div>
-          <div class="col-9">
-            <h5 class="font-weight-normal clearfix">
-              <a href="./img/Eb9YpYgVcAALa8d.jpg">XXXXXXX.jpg</a>
-              <small class="float-right">
-                <a href="#" class="btn btn-outline-danger btn-sm btn-del-attachment">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                  </svg>削除
-                </a>
-              </small>
-            </h5>
-            <div class="form-inline">
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                <label class="custom-control-label" for="inlineRadio1">1</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                <label class="custom-control-label" for="inlineRadio2">2</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" disabled>
-                <label class="custom-control-label" for="inlineRadio3">3 (disabled)</label>
-              </div>
-            </div>
-          </div>
-        </div>
+    <ul class="list-unstyled" id="product-list">
+      <li class="py-2 d-flex">
+        <input type="text" name="product-id" class="product-id form-control d-inline-block mr-3" value="1" readonly>
+        <input type="text" name="title" class="title form-control d-inline-block">
       </li>
-      <li class="my-3">
-        <div class="row">
-          <div class="col-3 d-flex justify-content-center">
-            <!-- 画像ファイル -->
-            <img src="./img/Eb9YpYgVcAALa8d.jpg" alt="" class="img-thumbnail border-0">
-          </div>
-          <div class="col-9">
-            <h5 class="font-weight-normal clearfix">
-              <a href="./img/Eb9YpYgVcAALa8d.jpg">XXXXXXX.jpg</a>
-              <small class="float-right">
-                <a href="#" class="btn btn-outline-danger btn-sm btn-del-attachment">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                  </svg>削除
-                </a>
-              </small>
-            </h5>
-            <div class="form-inline">
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions2" id="inlineRadio1-2" value="option1">
-                <label class="custom-control-label" for="inlineRadio1">1</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2-2" value="option2">
-                <label class="custom-control-label" for="inlineRadio2">2</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions2" id="inlineRadio3-2" value="option3" disabled>
-                <label class="custom-control-label" for="inlineRadio3">3 (disabled)</label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="my-3">
-        <div class="row">
-          <div class="col-3 d-flex justify-content-center">
-            <!-- PDF -->
-            <div class="embed-responsive embed-responsive-a4">
-              <embed src="./img/riyou.pdf#page=1" type="application/pdf" title="XXXXXX.pdf" class="embed-responsive-item">
-            </div>
-          </div>
-          <div class="col-9">
-            <h5 class="font-weight-normal clearfix">
-              <a href="./img/Eb9YpYgVcAALa8d.jpg">XXXXXXX.jpg</a>
-              <small class="float-right">
-                <a href="#" class="btn btn-outline-danger btn-sm btn-del-attachment">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                  </svg>削除
-                </a>
-              </small>
-            </h5>
-            <div class="form-inline">
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions3" id="inlineRadio1-3" value="option1">
-                <label class="custom-control-label" for="inlineRadio1">1</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions3" id="inlineRadio2-3" value="option2">
-                <label class="custom-control-label" for="inlineRadio2">2</label>
-              </div>
-              <div class="custom-control custom-radio mr-3">
-                <input class="custom-control-input" type="radio" name="inlineRadioOptions3" id="inlineRadio3-3" value="option3" disabled>
-                <label class="custom-control-label" for="inlineRadio3">3 (disabled)</label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ol>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Last Name</th>
-          <th>First Name</th>
-          <th>Email</th>
-          <th>Due</th>
-          <th>Web Site</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Smith</td>
-          <td>John</td>
-          <td>jsmith@gmail.com</td>
-          <td>$50.00</td>
-          <td>http://www.jsmith.com</td>
-        </tr>
-        <tr>
-          <td>Bach</td>
-          <td>Frank</td>
-          <td>fbach@yahoo.com</td>
-          <td>$50.00</td>
-          <td>http://www.frank.com</td>
-        </tr>
-        <tr>
-          <td>Doe</td>
-          <td>Jason</td>
-          <td>jdoe@hotmail.com</td>
-          <td>$100.00</td>
-          <td>http://www.jdoe.com</td>
-        </tr>
-        <tr>
-          <td>Conway</td>
-          <td>Tim</td>
-          <td>tconway@earthlink.net</td>
-          <td>$50.00</td>
-          <td>http://www.timconway.com</td>
-        </tr>
-      </tbody>
-    </table>
+    </ul>
   </div>
   <script src="./js/jquery-3.5.1.min.js"></script>
   <script src="./js/bootstrap.bundle.min.js"></script>
@@ -177,27 +28,44 @@
   <script src="./js/script.js"></script>
   <script>
     $(function(){
-      $(".delivery-datepicker").flatpickr({
-        minDate: new Date().fp_incr(1)
-      });
-      $(".order-datepicker").flatpickr({
-        defaultDate: "today",
-        onChange: function(selectedDates, dateStr){
-          console.log(dateStr);
-          const minDate = dateStr.split('-');
-          flatpickr(".delivery-datepicker", {
-            minDate: new Date(minDate[0], minDate[1] - 1, minDate[2]).fp_incr(1)
-          });
+      let mutationObserver = new MutationObserver(function(){
+        if (!$("#product-list li").length) {
+          mutationObserver.disconnect();
         }
+        $("#product-list li").each(function(index){
+          $.ajax({
+            url: "./search.php",
+            type: 'GET',
+            dataType: 'json',
+            timeout: 10000,
+            data: {id: $(".product-id", this).val()}
+          }).then(
+            function(data){
+              $("#product-list li").eq(index).find('.title').val(data[0].title).prop('readonly', true);
+            },
+            function(jqxhr, status, exception){
+              console.log(jqxhr);
+              console.log(status);
+              console.log(exception);
+            }
+          );
+        });
       });
+      const config = {
+        attributes: true,
+        childList: true
+      }
+      mutationObserver.observe(
+        // target
+        document.getElementById('product-list'),
+        config
+      );
 
-      $(".btn-del-attachment").on('click', function(){
-        if (confirm("添付ファイルを完全に削除しますか？この操作は取り消せません。")) {
-          $(this).closest("li").remove();
-        }
-      });
-
-      $(".table").tablesorter();
+      setInterval(() => {
+        const $clone = $("#product-list li").last().clone(true);
+        $clone.find('.product-id').val(Math.ceil(Math.random() * 10));
+        $("#product-list").append($clone);
+      }, 5000);
     });
   </script>
 </body>
--- search.php
+++ search.php
@@ -4,37 +4,35 @@
 	
 	try {
 
-		$query = "SELECT id, title, author, publisher, date, price FROM books";
+		$query = "SELECT * FROM articles";
 		// ユーザーの入力からSQLを組み立てる
 		$conditions = [];
 		$bind_param = [];
 
-		if (!empty($_POST['id'])) {
-				$conditions[] = "id LIKE ?";
-				$bind_param[] = '%'.$_POST['id'].'%';
-		}
-
-		if (!empty($_POST['title'])) {
-			$conditions[] = "title LIKE ?";
-			$bind_param[] = '%'.$_POST['title'].'%';
+		if (isset($_GET['id'])) {
+			$ids = explode(",", $_GET['id']);
+			$conditions[] = sprintf("id IN (%s)", substr(str_repeat(",?", count($ids)), 1));
+			$bind_param[] = $ids;
 		}
 
 		if (count($conditions) > 0) {
-				$query .= ' WHERE ' . join(' OR ', $conditions);
+			$query .= ' WHERE ' . join(' OR ', $conditions);
 		}
 
 		$stmt = $pdo -> prepare($query);
-		$stmt -> execute($bind_param);
+		$stmt -> execute($bind_param[0]);
 
 		$bookList = [];
 		while($book = $stmt -> fetch(PDO::FETCH_ASSOC)){
 			$bookList[] = array(
-				'id' 				=> $book['id'],
-				'title' 		=> $book['title'],
-				'author' 		=> $book['author'],
-				'publisher' => $book['publisher'],
-				'date' 			=> $book['date'],
-				'price' 		=> $book['price']
+				'id' 					=> $book['id'],
+				'user_id' 		=> $book['user_id'],
+				'title' 			=> $book['title'],
+				'slug' 				=> $book['slug'],
+				'body' 				=> $book['body'],
+				'published' 	=> $book['published'],
+				'created'			=> $book['created'],
+				'modified' 		=> $book['modified'],
 			);
 		}
 		header('Content-type: application/json');
