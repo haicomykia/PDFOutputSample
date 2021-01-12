26ebaecae4550de4c720bf83edd6d2d891c23b22
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-01-12 23:11:09 +0900

    複数行の出力に対応

--- pdf.php
+++ pdf.php
@@ -51,7 +51,7 @@
 	// フッターの出力を無効化
 	$pdf->setPrintFooter(false);
 	$pdf->SetMargins(0, 0, 0);
-	$pdf->SetCellPadding(0);
+	$pdf->SetCellPaddings(2, 0, 2, 0);
 	
 	$pdf->setSourceFile('./img/base.pdf');
 	$pdf->AddPage();
@@ -68,17 +68,24 @@
 	// 	$pdf->Line(0, $y, 210, $y);
 	// }
 
+	$here_doc = <<< EOM
+	この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れてい
+	EOM;
 	foreach($articles as $key => $article) {
-		$y = 50 + ($key * 6);
-		$pdf -> Text(24.5, $y, date('Y/m/d'));
-		$pdf -> Text(53, $y, $article['title']);
+		$y = 51 + ($key * 8);
 
-		$pdf -> SetXY(154, $y);
-		$pdf->Cell(33, 6, $key.'円', 0, 0, 'R');
+		$pdf -> SetXY(20, $y);
+		$pdf->setCellHeightRatio(2);  
+		$pdf -> Cell(32, 2, date('Y-m-d'), 0, 0, 'C');
 
+		$pdf -> SetXY(52, $y);
+		$pdf -> Cell(105, 2, $article['title'], 0, 0, 'L');
 	}
 
-	$image = __DIR__.'/img/stamp.gif';
-	$pdf -> Image($image, 160, 90, 30, 0, 'GIF');
+	$pdf -> SetXY(54, $y);
+	$pdf -> MultiCell(170, 55, $here_doc, 0, 'J', false, 0 ,20,125, true, 0, false, true, 55, 'T', false);
+
+	// $image = __DIR__.'/img/stamp.gif';
+	// $pdf -> Image($image, 160, 90, 30, 0, 'GIF');
 	$file_name = date('ymd').'_sample.pdf';
 	$pdf->Output($file_name, 'I'); // 画面出力
