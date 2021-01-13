1b190b6aefc02cf26d87f8e51eccd80e5372652f
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-01-13 21:00:02 +0900

    複数ページに対応

--- pdf.php
+++ pdf.php
@@ -20,7 +20,7 @@
 			$query .= ' WHERE ' . join(' OR ', $conditions);
 		}
 
-		$query .= "ORDER BY title DESC";
+		$query .= "ORDER BY id ASC";
 
 		$stmt = $pdo -> prepare($query);
 		$stmt -> execute($bind_param[0]);
@@ -46,45 +46,55 @@
 	use setasign\Fpdi\TcpdfFpdi;
 
 	$pdf = new TcpdfFpdi('P', 'mm', 'A4');
-	// ヘッダーの出力を無効化
-	$pdf->setPrintHeader(false);
-	// フッターの出力を無効化
-	$pdf->setPrintFooter(false);
-	$pdf->SetMargins(0, 0, 0);
-	$pdf->SetCellPaddings(2, 0, 2, 0);
+
+	define('NUMBER_OF_PRODUCTS_PER_PAGE', 7);
+
+	// ページごとの処理
+	for ($i = 0; $i < ceil(count($articles) / NUMBER_OF_PRODUCTS_PER_PAGE); $i++) { 
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+		$pdf->SetMargins(0, 0, 0);
+		$pdf->SetCellPaddings(2, 0, 2, 0);
+
+		$pdf->setSourceFile('./img/base.pdf');
+		$pdf->AddPage();
+		$tpl = $pdf->importPage(1); // テンプレートPDFの1ページ目
+		$pdf->useTemplate($tpl);
+		$pdf->SetFont('kozminproregular', '', 11);
+
+		// A4サイズの用紙にグリッドを引く
+		// for ($x = 10; $x < 210; $x += 10) {
+		// 	$pdf->Line($x, 0, $x, 297);
+		// }
+		// for ($y = 10; $y < 297; $y += 10) {
+		// 	$pdf->Line(0, $y, 210, $y);
+		// }
+		
+		// 明細に出力
+		for ($j = 0; $j < NUMBER_OF_PRODUCTS_PER_PAGE; $j++) {
+			$idx = NUMBER_OF_PRODUCTS_PER_PAGE * $i + $j;
+			$article = $articles[$idx];
+			$y = 51 + ($j * 8);
+
+			$pdf -> SetXY(20, $y);
+			$pdf->setCellHeightRatio(2);  
+			$pdf -> Cell(32, 2, date('Y-m-d'), 0, 0, 'C');
 	
-	$pdf->setSourceFile('./img/base.pdf');
-	$pdf->AddPage();
-	$tpl = $pdf->importPage(1); // テンプレートPDFの1ページ目
-	$pdf->useTemplate($tpl);
+			$pdf -> SetXY(52, $y);
+			$pdf -> Cell(105, 2, $article['title'], 0, 0, 'L');
 
-	$pdf->SetFont('kozminproregular', '', 11);
-
-	// A4サイズの用紙にグリッドを引く
-	// for ($x = 10; $x < 210; $x += 10) {
-	// 	$pdf->Line($x, 0, $x, 297);
-	// }
-	// for ($y = 10; $y < 297; $y += 10) {
-	// 	$pdf->Line(0, $y, 210, $y);
-	// }
-
-	$here_doc = <<< EOM
-	この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れてい
-	EOM;
-	foreach($articles as $key => $article) {
-		$y = 51 + ($key * 8);
-
-		$pdf -> SetXY(20, $y);
-		$pdf->setCellHeightRatio(2);  
-		$pdf -> Cell(32, 2, date('Y-m-d'), 0, 0, 'C');
-
-		$pdf -> SetXY(52, $y);
-		$pdf -> Cell(105, 2, $article['title'], 0, 0, 'L');
+			// 次の要素がない場合は明細の作成を終える
+			if (!isset($articles[$idx + 1])) {
+				break;
+			}
+		}
+		$here_doc = <<< EOM
+		この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れてい
+		EOM;
+		$pdf -> SetXY(54, $y);
+		$pdf -> MultiCell(170, 55, $here_doc, 0, 'J', false, 0 ,20,125, true, 0, false, true, 55, 'T', false);
+		$pdf -> lastPage();
 	}
-
-	$pdf -> SetXY(54, $y);
-	$pdf -> MultiCell(170, 55, $here_doc, 0, 'J', false, 0 ,20,125, true, 0, false, true, 55, 'T', false);
-
 	// $image = __DIR__.'/img/stamp.gif';
 	// $pdf -> Image($image, 160, 90, 30, 0, 'GIF');
 	$file_name = date('ymd').'_sample.pdf';
