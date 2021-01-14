4ba177545eee327d8796587cdeb90cd9853900c2
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-01-14 21:29:03 +0900

    PhpspreadSheetを別ファイルに移動

---
+++ excel.php
@@ -0,0 +1,62 @@
+<?php
+	require_once 'components/ArticleCollector.php';
+	require_once './components/vendor/autoload.php';
+	require_once 'connect_to_db.php';
+
+	use PhpOffice\PhpSpreadsheet\Spreadsheet;
+	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+	try {
+		$query = "SELECT * FROM articles";
+		// ユーザーの入力からSQLを組み立てる
+		$conditions = [];
+		$bind_param = [];
+
+		if (!empty($_GET['id'])) {
+			$ids = explode(",", $_GET['id']);
+			$conditions[] = sprintf("id IN (%s)", substr(str_repeat(",?", count($ids)), 1));
+			$bind_param[] = $ids;
+		}
+
+		if (count($conditions) > 0) {
+			$query .= ' WHERE ' . join(' OR ', $conditions);
+		}
+
+		$query .= " ORDER BY id ASC";
+
+		$stmt = $pdo -> prepare($query);
+		if (count($conditions) > 0) {
+			$stmt -> execute($bind_param[0]);
+		} else {
+			$stmt -> execute();
+		}
+
+		$articles = [];
+		while($article = $stmt -> fetch(PDO::FETCH_ASSOC)){
+			$articles[] = $article;
+		}
+	} catch (Exception $e) {
+		var_dump($e -> getMessage());
+		die('データの取得に失敗しました.');
+	}
+
+	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('./img/forms.xlsx');
+	$worksheet = $spreadsheet->getActiveSheet();
+
+	$worksheet->fromArray($articles, null, 'B5');
+
+	$cloned = clone $spreadsheet->getSheetByName('Sheet1');
+	$cloned->setTitle('送付状');
+	$spreadsheet->addSheet($cloned);
+	$sheet = $spreadsheet -> setActiveSheetIndex(1);
+	$sheet->setCellValue('D3', 'AAAAAAA');
+	
+	// redirect output to client browser
+	header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+	header('Content-Disposition: attachment;filename="written.xlsx"');
+	header('Cache-Control: max-age=0');
+
+	$writer = new Xlsx($spreadsheet);
+	$writer->save('php://output');
+	exit;
+	
